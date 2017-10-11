8a7dac46514318d463cbf1dc053229a8e946a880
Student <Student@ZAPRE-D00000151.DirectDelivery.com>
2017-10-11 11:07:26 +0200

    Call22 project

---
+++ NetBeansProjects/Cal22/src/cal22/Cal22.java
@@ -0,0 +1,24 @@
+package cal22;
+
+/**
+ *
+ * @author Student
+ */
+public class Cal22 
+{
+
+    public int add (String s1 , String s2)
+    {
+        
+        return 0;
+    }
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
